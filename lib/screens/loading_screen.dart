e7bce4f5aff849c7e58163bc23349c4bd9f971d1
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-24 20:12:43 +0900

    add: LoadingScreenにinitStateを追加
    refs #4 画面を表示したらまずユーザーの位置情報を取得

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,6 +7,14 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    getLocation();
+  }
+
   void getLocation() async {
     LocationPermission permission = await Geolocator.requestPermission();
     Position position = await Geolocator.getCurrentPosition(
@@ -16,15 +24,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
