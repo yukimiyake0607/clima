54ca308aecb8c80069ce8b120207b29b974fe2b7
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-27 15:32:45 +0900

    add: アプリを立ち上げると位置情報を取得し、温度や地名をコンソールに出力
    refs #10

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,10 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const String apiKey = 'faca477592cf21c0f3627e68fc1fd1b0';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,6 +12,9 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longitude;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -17,20 +24,36 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    // ignore: unused_local_variable
-    LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(Uri.parse(
-        'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=%3Cspan%20class='));
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
 
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+
+      var decodeData = jsonDecode(data);
+
+      double temperature = decodeData['main']['temp'];
+      int condition = decodeData['weather'][0]['id'];
+      String cityName = decodeData['name'];
+      final lat = decodeData['coord']['lat'];
+      final lon = decodeData['coord']['lon'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+      print(lat);
+      print(lon);
     } else {
       print(response.statusCode);
     }
@@ -38,7 +61,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold();
   }
 }
