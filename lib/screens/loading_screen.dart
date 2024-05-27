0ab18d80dfb2543520cf2c845f51c396c4822c86
yukimiyake0607 <yukimiyake38@gmail.com>
2024-05-27 15:53:01 +0900

    refactor: コードの改善
    refs #12

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,7 @@
 import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 const String apiKey = 'faca477592cf21c0f3627e68fc1fd1b0';
 
@@ -20,10 +19,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // TODO: implement initState
     super.initState();
 
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
 
     await location.getCurrentLocation();
@@ -31,32 +30,10 @@ class _LoadingScreenState extends State<LoadingScreen> {
     latitude = location.latitude;
     longitude = location.longitude;
 
-    getData();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
-
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      var decodeData = jsonDecode(data);
-
-      double temperature = decodeData['main']['temp'];
-      int condition = decodeData['weather'][0]['id'];
-      String cityName = decodeData['name'];
-      final lat = decodeData['coord']['lat'];
-      final lon = decodeData['coord']['lon'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-      print(lat);
-      print(lon);
-    } else {
-      print(response.statusCode);
-    }
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    
+    var weatherData = await networkHelper.getData();
   }
 
   @override
@@ -64,3 +41,5 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold();
   }
 }
+
+
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -74,3 +74,10 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
+
+
+// double temperature = decodeData['main']['temp'];
+// int condition = decodeData['weather'][0]['id'];
+// String cityName = decodeData['name'];
+// final lat = decodeData['coord']['lat'];
+// final lon = decodeData['coord']['lon'];
